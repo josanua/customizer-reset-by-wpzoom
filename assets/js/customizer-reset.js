ac7c94bd41961a19a8794089aae3e60c1e152dfe
Josanu Andrei <andrei@quadra.md>
2024-07-27 09:49:23 +0300

    Use of const and let: Use const and let instead of var for better scoping.

--- assets/js/customizer-reset.js
+++ assets/js/customizer-reset.js
@@ -1,33 +1,33 @@
 /* global jQuery, _ZoomCustomizerReset, ajaxurl, wp */
 
 jQuery(function ($) {
-    var $container = $('#customize-header-actions');
-    var $button = $('<input type="submit" name="zoom-reset" id="zoom-reset" class="button-secondary button">')
-        .attr('value', _ZoomCustomizerReset.reset)
-        .css({
-            'float': 'right',
-            'margin-right': '10px',
-            'margin-top': '9px'
-        });
-    $button.on('click', function (event) {
-        event.preventDefault();
+	let $container = $('#customize-header-actions');
+	let $button = $('<input type="submit" name="zoom-reset" id="zoom-reset" class="button-secondary button">')
+		.attr('value', _ZoomCustomizerReset.reset)
+		.css({
+			'float': 'right',
+			'margin-right': '10px',
+			'margin-top': '9px'
+		});
+	$button.on('click', function (event) {
+		event.preventDefault();
 
-        var data = {
-            wp_customize: 'on',
-            action: 'customizer_reset',
-            nonce: _ZoomCustomizerReset.nonce.reset
-        };
+		let data = {
+			wp_customize: 'on',
+			action: 'customizer_reset',
+			nonce: _ZoomCustomizerReset.nonce.reset
+		};
 
-        var response = confirm(_ZoomCustomizerReset.confirm);
+		const response = confirm(_ZoomCustomizerReset.confirm);
 
-        if (!response) return;
+		if (!response) return;
 
-        $button.attr('disabled', 'disabled');
+		$button.attr('disabled', 'disabled');
 
-        $.post(ajaxurl, data, function () {
-            wp.customize.state('saved').set(true);
-            location.reload();
-        });
-    });
-    $container.append($button);
+		$.post(ajaxurl, data, function () {
+			wp.customize.state('saved').set(true);
+			location.reload();
+		});
+	});
+	$container.append($button);
 });
